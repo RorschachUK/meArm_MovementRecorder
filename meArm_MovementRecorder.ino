462c198431643fabbf1793f564a21c78bf84c2e0
RorschachUK <rorschachuk@gmail.com>
2014-06-15 15:43:49 +0100

    Added -/+ buttons

--- meArm_MovementRecorder.ino
+++ meArm_MovementRecorder.ino
@@ -87,23 +87,31 @@
 
 //Class holding one instruction
 class instruction {
-  public:
-    instruction(bool grip, float x, float y, float z) {
-      _type = "g";
-      _x=x; _y=y; _z=z;
-      _gripper = grip;
-    }
-    instruction(float x, float y, float z) {
-      _type="m";
-      _x=x; _y=y; _z=z; _gripper = false;
-    }
-    instruction() {
-      _type="";
-      _x=0; _y=0; _z=0; _gripper = false;
-    }
-    String _type;
-    float _x,_y,_z;
-    bool _gripper;
+public:
+  instruction(bool grip, float x, float y, float z) {
+    _type = "g";
+    _x=x; 
+    _y=y; 
+    _z=z;
+    _gripper = grip;
+  }
+  instruction(float x, float y, float z) {
+    _type="m";
+    _x=x; 
+    _y=y; 
+    _z=z; 
+    _gripper = false;
+  }
+  instruction() {
+    _type="";
+    _x=0; 
+    _y=0; 
+    _z=0; 
+    _gripper = false;
+  }
+  String _type;
+  float _x,_y,_z;
+  bool _gripper;
 };
 
 //hardware instances
@@ -117,6 +125,51 @@ QueueList<instruction> instructions;
 //Menu control
 int cursor = 0;
 bool lastButtonPushed = true;
+
+void clear() {
+ while(!instructions.isEmpty())
+   instructions.pop();
+}
+
+void replay() {
+  //reset to start state
+  arm.gotoPoint(0,100,50);
+  arm.openGripper();
+  //go through steps
+  for (int i=0; i< instructions.count(); i++) {
+    instruction instr = instructions.pop();
+    //Update UI
+    nokia.fillRect(0,32, 84, 16, WHITE);
+    nokia.setCursor(0,32);
+    nokia.print(F("Replay step "));
+    nokia.print(i+1);
+    nokia.setCursor(0,40);
+    nokia.print("(");
+    nokia.print(int(instr._x));
+    nokia.print(",");
+    nokia.print(int(instr._y));
+    nokia.print(",");
+    nokia.print(int(instr._z));
+    nokia.print(")");
+    nokia.display();
+    if (instr._type == "g")
+      if (instr._gripper) {
+        arm.gotoPoint(instr._x, instr._y, instr._z);
+        arm.openGripper();
+      } 
+      else {
+        arm.gotoPoint(instr._x, instr._y, instr._z);
+        arm.closeGripper();
+      }
+    else if (instr._type == "m")
+      arm.gotoPoint(instr._x, instr._y, instr._z);
+    //put back on queue
+    instructions.push(instr);
+  }
+  //reset to start state
+  arm.gotoPoint(0,100,50);
+  arm.openGripper();
+}
 
 void setup() {
   //Setup static elements of UI
@@ -138,7 +191,7 @@ void setup() {
 #else
   arm.begin(11,10,9,6);
 #endif
-  
+
   arm.openGripper();
 
   cc.begin(WII_PLAYER_1);
@@ -155,11 +208,12 @@ void loop() {
   float dg = 0;
   //certain buttons should not repeat continually
   bool currentButton = cc.button_up() || cc.button_down() || cc.button_home() 
-       || cc.button_a() || cc.button_b() || cc.shoulder_right() > 16 
-       || cc.shoulder_left() > 16;
+    || cc.button_a() || cc.button_b() || cc.button_plus_start()
+    || cc.button_minus_select() || cc.shoulder_right() > 16 || cc.shoulder_left() > 16;
   if (lastButtonPushed && !currentButton) {
     lastButtonPushed = false;
-  } else if (currentButton && !lastButtonPushed) {
+  } 
+  else if (currentButton && !lastButtonPushed) {
     lastButtonPushed = true;
     if (cc.button_home()) {
       //reset to start point
@@ -169,55 +223,22 @@ void loop() {
     if (cc.button_a() || cc.button_b()) {
       //Menu choice button pushed
       switch(cursor) {
-        case 0: { // save instruction
-            instruction newInstr(arm.getX(), arm.getY(), arm.getZ());
-            instructions.push(newInstr);
-            break;
-          }
-        case 1: { // clear instructions
-          while(!instructions.isEmpty())
-            instructions.pop();
+      case 0: 
+        { // save instruction
+          instruction newInstr(arm.getX(), arm.getY(), arm.getZ());
+          instructions.push(newInstr);
           break;
-          }
-        case 2: { // replay instructions
-            //reset to start state
-            arm.gotoPoint(0,100,50);
-            arm.openGripper();
-            //go through steps
-            for (int i=0; i< instructions.count(); i++) {
-              instruction instr = instructions.pop();
-              //Update UI
-              nokia.fillRect(0,32, 84, 16, WHITE);
-              nokia.setCursor(0,32);
-              nokia.print(F("Replay step "));
-              nokia.print(i+1);
-              nokia.setCursor(0,40);
-              nokia.print("(");
-              nokia.print(int(instr._x));
-              nokia.print(",");
-              nokia.print(int(instr._y));
-              nokia.print(",");
-              nokia.print(int(instr._z));
-              nokia.print(")");
-              nokia.display();
-              if (instr._type == "g")
-                if (instr._gripper) {
-                  arm.gotoPoint(instr._x, instr._y, instr._z);
-                  arm.openGripper();
-                } else {
-                  arm.gotoPoint(instr._x, instr._y, instr._z);
-                  arm.closeGripper();
-                }
-              else if (instr._type == "m")
-                arm.gotoPoint(instr._x, instr._y, instr._z);
-              //put back on queue
-              instructions.push(instr);
-            }
-            //reset to start state
-            arm.gotoPoint(0,100,50);
-            arm.openGripper();
-            break;
-          }
+        }
+      case 1: 
+        { // clear instructions
+          clear();
+          break;
+        }
+      case 2: 
+        { // replay instructions
+          replay();
+          break;
+        }
       }          
     }
     //Menu movements
@@ -225,12 +246,17 @@ void loop() {
       cursor--;
     else if (cc.button_down() && cursor < 2)
       cursor++;
+    if (cc.button_plus_start())
+      replay();
+    if (cc.button_minus_select())
+      clear();
     //gripper control
     if (cc.shoulder_left() > 16) {
       arm.openGripper();
       instruction newInstr(true, arm.getX(), arm.getY(), arm.getZ());
       instructions.push(newInstr);
-    } else if (cc.shoulder_right() > 16) {
+    } 
+    else if (cc.shoulder_right() > 16) {
       arm.closeGripper();
       instruction newInstr(false, arm.getX(), arm.getY(), arm.getZ());
       instructions.push(newInstr);
@@ -272,6 +298,7 @@ void loop() {
   nokia.print(int(arm.getZ()));
   nokia.print(")");
   nokia.display();
- 
+
   delay(20);
 }
+
